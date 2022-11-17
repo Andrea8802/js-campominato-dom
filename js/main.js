9ff6859c6a8f198ec3136cbf2a2d7aa6f7d3d61b
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-17 15:53:59 +0100

    All Project Completed

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ const genera = document.getElementById("play");
 const difficolta = document.getElementById("difficolta");
 const reset = document.getElementById("reset");
 let elements = document.getElementsByClassName("box");
+const punteggio = document.getElementById("punteggio");
+
+// Dichiariazione variabili
+let esploso = false;
+let score = 0;
+
 
 // Array
 let arrNum = [];
@@ -53,17 +59,37 @@ function creaBox(nBox, modalita){
         box.classList.add(modalita);
         container.append(box);
         box.innerHTML = i;
-        arrNum = bombNumber(1, 100, 16);
+        arrNum = bombNumber(1, 100, 16)
+
 
         // Click sui numeri
         box.addEventListener("click",
             function(){
-                console.log(i);
-                if(arrNum.includes(i)){
-                    box.classList.add("bomba");
-                } else{
-                    box.classList.add("safe");
-                }    
+
+                // Se non è esploso allora
+                if (esploso === false){
+
+                    // Se clicchi sulla cella "bomba" perdi
+                    if (arrNum.includes(i)){
+                        box.classList.add("bomba");
+                        esploso = true;
+
+                    }  else if (box.classList[2] != "safe") {
+                        // ALtrimenti Se la cella selezionata non lo è già
+                        box.classList.add("safe");
+                        score += 1;
+                        punteggio.innerHTML = "Punteggio: " + score;
+
+                        // Se tutte le celle safe sono selezionate vinci
+                        if (score === nBox - arrNum.length){
+                            alert("Hai vinto!")
+                        }
+                    } 
+
+                } else if (esploso === true){
+                    score = 0;
+                    alert("Hai perso! Resetta la parita per continuare");
+                }   
             }
         )
     }
